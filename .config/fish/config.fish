985108265d89f2a23881a528b291a57bd039b5de
Jboss-10 <jonathanbuss08@gmail.com>
2025-04-09 23:51:34 +0100

    make fzf show hidden files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -172,6 +172,7 @@ end
 # fzf
 fzf --fish | source
 fzf_configure_bindings --directory=\cf --variables=\e\cv
+set fzf_fd_opts --hidden --exclude=.git
 
 # Personal Aliases
 alias todo 'todoist s ; todoist l'
