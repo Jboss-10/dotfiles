a6d1a1f5b14bb129450b1e5be8c2b75c19d64c6a
Jboss-10 <jonathanbuss08@gmail.com>
2025-04-10 18:17:39 +0100

    lt nolonger shows .git

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -105,7 +105,7 @@ end
 alias ls 'eza -al --color=always --group-directories-first --icons' # preferred listing
 alias la 'eza -a --color=always --group-directories-first --icons' # all files and dirs
 alias ll 'eza -l --color=always --group-directories-first --icons' # long format
-alias lt 'eza -aT --color=always --group-directories-first --icons' # tree listing
+alias lt 'eza -aT --color=always --group-directories-first --icons -I .git' # tree listing
 alias l. 'eza -ald --color=always --group-directories-first --icons .*' # show only dotfiles
 
 # Replace some more things with better alternatives
